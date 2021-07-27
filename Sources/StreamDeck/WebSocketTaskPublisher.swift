e2ae3020e693c085a34ae107db7d37f492d7001a
Emory Dunn <emory@emorydunn.com>
2021-07-26 21:33:30 -0700

    Rename the receiver

--- Sources/StreamDeck/WebSocketTaskPublisher.swift
+++ Sources/StreamDeck/WebSocketTaskPublisher.swift
@@ -45,7 +45,11 @@ public struct WebSocketTaskPublisher: Publisher {
             .receive(subscription: subscrption)
 
     }
-
+    
+//    public func send(_ message: URLSessionWebSocketTask.Message) {
+//        task.send(message, completionHandler: <#T##(Error?) -> Void#>)
+//    }
+    
 }
 
 extension WebSocketTaskPublisher {
@@ -65,15 +69,11 @@ extension WebSocketTaskPublisher {
             // Resume the task
             task.resume()
             
-            sendReceivedEvents(to: target, with: demand)
+            listen(for: target, with: demand)
             
         }
         
-        /// Receive events from the WebSocket and pass them along to the subscriber.
-        /// - Parameters:
-        ///   - target: The subscriber to attach to this Publisher, after which it can receive values.
-        ///   - demand: A requested number of items, sent to a publisher from a subscriber through the subscription.
-        func sendReceivedEvents(to target: Target, with demand: Subscribers.Demand) {
+        func listen(for target: Target, with demand: Subscribers.Demand) {
             var demand = demand
             
             self.task.receive { [weak self] result in
@@ -86,7 +86,7 @@ extension WebSocketTaskPublisher {
                 }
                 
                 if demand > 0 {
-                    self?.sendReceivedEvents(to: target, with: demand)
+                    self?.listen(for: target, with: demand)
                 }
             }
         }
