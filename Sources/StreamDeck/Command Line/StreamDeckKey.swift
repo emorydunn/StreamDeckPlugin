ea9b4ebfdef1a57aa62e64b0ed6706ca392b11db
Emory Dunn <emory@emorydunn.com>
2022-12-26 14:34:09 -0800

    Add `layout` and `background` overrides

--- Sources/StreamDeck/Command Line/StreamDeckKey.swift
+++ Sources/StreamDeck/Command Line/StreamDeckKey.swift
@@ -10,28 +10,30 @@ import ArgumentParser
 
 /// A coding key for converting between `JSONCodable` and the keys expected by the Stream Deck application.
 struct StreamDeckKey: CodingKey {
-    var stringValue: String
-    var intValue: Int?
-    
-    init(key: CodingKey) {
-        self.init(stringValue: key.stringValue)!
-    }
-    
-    init?(stringValue: String) {
-        switch stringValue {
-        case "sdkVersion":
-            self.stringValue = "SDKVersion"
-        case "uuid", "url", "os":
-            self.stringValue = stringValue.uppercased()
-        default:
-            let firstLetter = stringValue.first!.uppercased()
-            self.stringValue = firstLetter + stringValue.dropFirst()
-        }
-        self.intValue = nil
-    }
-    
-    init?(intValue: Int) {
-        self.stringValue = String(intValue)
-        self.intValue = intValue
-    }
+	var stringValue: String
+	var intValue: Int?
+
+	init(key: CodingKey) {
+		self.init(stringValue: key.stringValue)!
+	}
+
+	init?(stringValue: String) {
+		switch stringValue {
+		case "sdkVersion":
+			self.stringValue = "SDKVersion"
+		case "uuid", "url", "os":
+			self.stringValue = stringValue.uppercased()
+		case "layout", "background":
+			self.stringValue = stringValue
+		default:
+			let firstLetter = stringValue.first!.uppercased()
+			self.stringValue = firstLetter + stringValue.dropFirst()
+		}
+		self.intValue = nil
+	}
+
+	init?(intValue: Int) {
+		self.stringValue = String(intValue)
+		self.intValue = intValue
+	}
 }
