671b2768139754989ddcbb4908955dc74b48b9c0
Emory Dunn <emory@emorydunn.com>
2023-08-18 11:21:21 -0700

    Mark as mutable

--- Sources/StreamDeck/Support Models/PluginManifest.swift
+++ Sources/StreamDeck/Support Models/PluginManifest.swift
@@ -341,38 +341,38 @@ struct PluginAction: Codable {
 public struct PluginActionState: Codable {
 
 	/// The default image for the state.
-	public let image: String
+	public var image: String
 
 	/// The name of the state displayed in the dropdown menu in the Multi action.
 	///
 	/// For example Start or Stop for the states of the Game Capture Record action. If the name is not provided, the state will not appear in the Multi Action.
-	public let name: String?
+	public var name: String?
 
 	/// Default title.
-	public let title: String?
+	public var title: String?
 
 	/// Boolean to hide/show the title. True by default.
-	public let showTitle: Bool?
+	public var showTitle: Bool?
 
 	/// Default title color.
-	public let titleColor: String?
+	public var titleColor: String?
 
 	/// Default title vertical alignment.
-	public let titleAlignment: Alignment?
+	public var titleAlignment: Alignment?
 
 	/// Default font family for the title.
-	public let fontFamily: FontFamily?
+	public var fontFamily: FontFamily?
 
 	/// Default font style for the title.
 	///
 	/// - Note: Some fonts might not support all values.
-	public let fontStyle: FontStyle?
+	public var fontStyle: FontStyle?
 
 	/// Default font size for the title.
-	public let fontSize: Int?
+	public var fontSize: Int?
 
 	/// Boolean to have an underline under the title. False by default
-	public let fontUnderline: Bool?
+	public var fontUnderline: Bool?
 
 	public init(image: String,
 				name: String? = nil,
