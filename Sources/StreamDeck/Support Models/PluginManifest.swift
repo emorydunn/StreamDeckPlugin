b5d7190db2d13c67e73dfd26a9057c7d9da85da6
Emory Dunn <emory@emorydunn.com>
2023-12-17 17:55:58 -0800

    Conform AppToMonitor to ExpressibleByArrayLiteral

--- Sources/StreamDeck/Support Models/PluginManifest.swift
+++ Sources/StreamDeck/Support Models/PluginManifest.swift
@@ -497,7 +497,7 @@ public struct PluginSoftware: Codable, ExpressibleByStringLiteral {
 /// In order to do so, the ApplicationsToMonitor object should contain for each platform an array specifying the list of application identifiers to monitor.
 ///
 /// - Note: On macOS the application bundle identifier is used while the exe filename is used on Windows.
-public struct ApplicationsToMonitor: Codable {
+public struct ApplicationsToMonitor: Codable, ExpressibleByArrayLiteral {
 
 	/// Mac applications to monitor
 	public let mac: [String]
@@ -509,6 +509,14 @@ public struct ApplicationsToMonitor: Codable {
 	public init(mac: [String] = [], windows: [String] = []) {
 		self.mac = mac
 		self.windows = windows
+	}
+	
+	/// Initialize new applications to monitor.
+	///
+	/// - Note: Using the array literal will set the same applications to both macOS and Windows. 
+	/// - Parameter elements: The applications to monitor.
+	public init(arrayLiteral elements: String...) {
+		self.init(mac: elements, windows: elements)
 	}
 }
 
